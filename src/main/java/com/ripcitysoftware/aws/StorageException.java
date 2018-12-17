a9c7484aaf6fc74be32b2cf5d24f5d78a7307d60
Chris Maki <chris.maki@ripcitysoftware.com>
2018-12-17 08:57:01 -0800

    remove unused methods

--- src/main/java/com/ripcitysoftware/aws/StorageException.java
+++ src/main/java/com/ripcitysoftware/aws/StorageException.java
@@ -2,14 +2,6 @@ package com.ripcitysoftware.aws;
 
 public class StorageException extends RuntimeException {
 
-  public StorageException() {
-    super();
-  }
-
-  public StorageException(String message) {
-    super(message);
-  }
-
   public StorageException(String message, Throwable cause) {
     super(message, cause);
   }
